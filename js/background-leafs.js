0983db888e6239da1aaca471099acd56c92d6840
Hlib Hraif <hlib.hraif@protonmail.com>
2020-09-21 15:36:03 +0300

    Add content, use and extend local Bootstrap copy

--- js/background-leafs.js
+++ js/background-leafs.js
@@ -6,32 +6,37 @@ const width = window.innerWidth
 const mobile = width < 768;
 const sidesWidth = 10;
 
+var leafs = document.querySelectorAll(".background-leaf");
+
 function calculateWidth(level) {
     return mobile ? level + levels : (level + levels) / 2;
 }
 
-const maximumWidth = calculateWidth(levels / 2 + 1);
-
-var leafs = document.querySelectorAll(".background-leaf");
-
-leafs.forEach(leaf => {
-    var right = Math.random() > 0.5;
-
-    var x;
-    if (mobile) {
-        x = Math.random() * (100 - maximumWidth);
-    } else {
-        x = (right ? (100 - sidesWidth - maximumWidth) : 0) + Math.random() * 10;
-    }
-
-    var y = Math.random() * 100;
-    var z = -Math.round(Math.random() * levels / 2 + 1);
+if (mobile) {
+    leafs.forEach(leaf => {
+        leaf.style.display = "none";
+    });
+} else {
+    const maximumWidth = calculateWidth(levels / 2 + 1);
     
-    if (!mobile)
+    leafs.forEach(leaf => {
+        var right = Math.random() > 0.5;
+    
+        // var x;
+        // if (mobile) {
+        //     x = Math.random() * (100 - maximumWidth);
+        // } else {
+        x = (right ? (100 - sidesWidth - maximumWidth) : 0) + Math.random() * 10;
+        // }
+    
+        var y = Math.random() * 100;
+        var z = -Math.round(Math.random() * levels / 2 + 1);
+        
         z = Math.random() > 0.5 ? z : -z;
-
-    leaf.style.cssText = `top: ${y}%; left: ${x}vw; z-index: ${z - levels};`;
-    leaf.style.width = `${calculateWidth(z)}vw`
-    leaf.dataset.rellaxSpeed = z;
-    leaf.dataset.rellaxPercentage = y/100;
-});
+    
+        leaf.style.cssText = `top: ${y}%; left: ${x}vw; z-index: ${z - levels};`;
+        leaf.style.width = `${calculateWidth(z)}vw`
+        leaf.dataset.rellaxSpeed = z;
+        leaf.dataset.rellaxPercentage = y/100;
+    });
+}
--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,8 @@
 window.onscroll = function () { checkPosition() };
-var carousel = document.getElementById("carousel");
+var first = document.getElementById("info");
 var navbar = document.getElementById("navbar");
 function checkPosition() {
-    if (window.pageYOffset >= carousel.offsetTop - 40) {
+    if (window.pageYOffset >= first.offsetTop - 40) {
         navbar.classList.add("opaque");
     } else {
         navbar.classList.remove("opaque");
